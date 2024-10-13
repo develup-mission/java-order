1d621dc4b8ee55d8e17cdb1a83988c94f989bf1c
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-13 15:18:56 +0900

    refactor: 배달비 상수 변수 이름 수정

--- src/main/java/order/global/constant/OrderPriceConstant.java
+++ src/main/java/order/global/constant/OrderPriceConstant.java
@@ -3,9 +3,9 @@ package order.global.constant;
 public enum OrderPriceConstant {
 
     MINIMUM_ORDER_PRICE(30000),
-    DELIVER_PRICE_FIRST(2000),
-    DELIVER_PRICE_SECOND(1000),
-    DELIVER_PRICE_THIRD(0);
+    DELIVERY_FEE_FIRST(2000),
+    DELIVERY_FEE_SECOND(1000),
+    DELIVERY_FEE_THIRD(0);
 
     private final int price;
 
