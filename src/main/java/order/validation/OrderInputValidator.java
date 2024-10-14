c5d0402c966438223cdb64aa27a028e3888e6897
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-14 14:25:19 +0900

    refactor: 미사용 클래스 삭제

--- src/main/java/order/validation/OrderInputValidator.java
+++
@@ -1,8 +0,0 @@
-package order.validation;
-
-public class OrderInputValidator {
-
-    public final String ORDER_MENU_SPLIT = ", ";
-    public final String ORDER_REGEX = "([가-힣a-zA-Z]+)\\((\\d+)개\\)";
-    public final int MAX_MENU_QUANTITY = 10;
-}
